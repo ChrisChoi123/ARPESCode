1774e49722d465f10b0ac63d63a3917b55afb750
Min Yeong Choi <mchoi00@stuy.edu>
2019-07-16 00:19:38 -0400

    added the data file, java, and class file

---
+++ Arpes.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Character;
+public class Arpes {
+  double[][] data;
+
+  public Arpes() {
+  }
+  public void loadData(String filename) throws FileNotFoundException  {
+    File text = new File(filename);
+    Scanner scan = new Scanner(text);
+    data = new double[1055][801];
+    int i = 0;
+    while(scan.hasNextLine()){
+      String line = scan.nextLine();
+      String[] nums = line.split("	");
+      for (int j = 0;j < nums.length;j++) {
+        if (i == 0 && j == 0) {
+          j++;
+        }
+        data[i][j] = Double.parseDouble(nums[j]);
+      }
+      i++;
+    }
+  }
+  public static void main(String[] args) {
+    try {
+      Arpes a = new Arpes();
+      a.loadData(args[0]);
+    }
+    catch(FileNotFoundException e){
+      System.out.println("Invalid text file");
+    }
+  }
+}
